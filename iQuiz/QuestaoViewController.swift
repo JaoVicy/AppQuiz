7064c60a6aeded8e21207584b93a122d66a17b57
JaoVicy <filipimjoa@gmail.com>
2025-06-07 19:13:40 -0300

    Added functionality to buttons

--- iQuiz/QuestaoViewController.swift
+++ iQuiz/QuestaoViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class QuestaoViewController: UIViewController {
 
-    @IBOutlet weak var tituloQuestaoLabel: UILabel!
+    var pontuacao: Int = 0
+    var numeroQuestao: Int = 0
     
+    @IBOutlet weak var tituloQuestaoLabel: UILabel!
     @IBOutlet var botoesRespostas: [UIButton]!
     
     @IBAction func respostaBotaoPressionado(_ sender: UIButton) {
+        
+        // A tag vai representar sempre um botão, se o botão clicado tiver a tag igual a variável de "respostaCorreta" é um acerto.
+        let usuarioAcertouResposta =
+        questoes[numeroQuestao].respostaCorreta == sender.tag
+        
+        if usuarioAcertouResposta {
+            pontuacao += 1
+            print("Correto!")
+        }
         
         print("O botão clicado tem a teg de: \(sender.tag)")
     }
@@ -23,13 +34,26 @@ class QuestaoViewController: UIViewController {
         super.viewDidLoad()
 
         configurarLayout()
+        configurarQuestao()
     }
     
     func configurarLayout() {
         navigationItem.hidesBackButton = true
+    
+        tituloQuestaoLabel.numberOfLines = 0
+        tituloQuestaoLabel.textAlignment = .center
         
         for botao in botoesRespostas {
             botao.layer.cornerRadius = 12.0
+        }
+    }
+    
+    func configurarQuestao() {
+        tituloQuestaoLabel.text = questoes[numeroQuestao].titulo
+        
+        for botao in botoesRespostas {
+            let tituloBotao = questoes[numeroQuestao].respostas[botao.tag]
+            botao.setTitle(tituloBotao, for: .normal)
         }
     }
     
