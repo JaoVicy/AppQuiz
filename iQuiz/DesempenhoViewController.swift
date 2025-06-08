748fa9c32fc2b61d607cbe3f31069fd30f18eda5
JaoVicy <filipimjoa@gmail.com>
2025-06-08 12:33:01 -0300

    Added final screen

---
+++ iQuiz/DesempenhoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DesempenhoViewController.swift
+//  iQuiz
+//
+//  Created by João Víctor Benetti Filipim on 08/06/25.
+//
+
+import UIKit
+
+class DesempenhoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
