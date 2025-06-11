98fe8c3035ee87c294986bf67be57e9091c5239b
JaoVicy <filipimjoa@gmail.com>
2025-06-11 15:25:42 -0300

    Fix questions

--- iQuiz/Questao.swift
+++ iQuiz/Questao.swift
@@ -21,38 +21,71 @@ let questoes: [Questao] = [
     Questao(
     titulo: "Que cor é cosiderada primaria?", respostas: ["Azul", "Roxo", "Rosa"], respostaCorreta: 0),
     Questao(
-        titulo: "Qual é o símbolo químico da água?", respostas: ["H2O", "CO2", "O2"], respostaCorreta: 0),
+        titulo: "Qual é o símbolo químico da água?",
+        respostas: ["CO2", "H2O", "O2"],
+        respostaCorreta: 1),
     Questao(
-        titulo: "Quem é o usuário do jutsu 'Chidori' no anime 'Naruto'?", respostas: ["Naruto", "Sasuke", "Sakura"], respostaCorreta: 1),
+        titulo: "Quem é o usuário do jutsu 'Chidori' no anime 'Naruto'?",
+        respostas: ["Sasuke", "Naruto", "Sakura"],
+        respostaCorreta: 0),
     Questao(
-        titulo: "Qual filósofo escreveu 'A República'?", respostas: ["Platão", "Aristóteles", "Sócrates"], respostaCorreta: 0),
+        titulo: "Qual filósofo escreveu 'A República'?",
+        respostas: ["Sócrates", "Platão", "Aristóteles"],
+        respostaCorreta: 1),
     Questao(
-        titulo: "Qual país é conhecido como a Terra do Sol Nascente?", respostas: ["Japão", "China", "Índia"], respostaCorreta: 0),
+        titulo: "Qual país é conhecido como a Terra do Sol Nascente?",
+        respostas: ["Índia", "Japão", "China"],
+        respostaCorreta: 1),
     Questao(
-        titulo: "Quem pintou a Mona Lisa?", respostas: ["Leonardo da Vinci", "Michelangelo", "Van Gogh"], respostaCorreta: 0),
+        titulo: "Quem pintou a Mona Lisa?",
+        respostas: ["Michelangelo", "Van Gogh", "Leonardo da Vinci"],
+        respostaCorreta: 2),
     Questao(
-        titulo: "Qual é o maior planeta do sistema solar?", respostas: ["Júpiter", "Saturno", "Marte"], respostaCorreta: 0),
+        titulo: "Qual é o maior planeta do sistema solar?",
+        respostas: ["Marte", "Júpiter", "Saturno"],
+        respostaCorreta: 1),
     Questao(
-        titulo: "Em que ano ocorreu a Revolução Francesa?", respostas: ["1789", "1804", "1492"], respostaCorreta: 0),
+        titulo: "Em que ano ocorreu a Revolução Francesa?",
+        respostas: ["1804", "1789", "1492"],
+        respostaCorreta: 1),
     Questao(
-        titulo: "Qual é a capital da Austrália?", respostas: ["Canberra", "Sydney", "Melbourne"], respostaCorreta: 0),
+        titulo: "Qual é a capital da Austrália?",
+        respostas: ["Melbourne", "Sydney", "Canberra"],
+        respostaCorreta: 2),
     Questao(
-        titulo: "Em ‘One Piece’, qual é o nome do protagonista?", respostas: ["Luffy", "Zoro", "Sanji"], respostaCorreta: 0),
+        titulo: "Em ‘One Piece’, qual é o nome do protagonista?",
+        respostas: ["Sanji", "Luffy", "Zoro"],
+        respostaCorreta: 1),
     Questao(
-        titulo: "Qual elemento tem o número atômico 6?", respostas: ["Carbono", "Oxigênio", "Nitrogênio"], respostaCorreta: 0),
+        titulo: "Qual elemento tem o número atômico 6?",
+        respostas: ["Oxigênio", "Carbono", "Nitrogênio"],
+        respostaCorreta: 1),
     Questao(
-        titulo: "Quem desenvolveu a teoria da relatividade?", respostas: ["Albert Einstein", "Isaac Newton", "Galileu Galilei"], respostaCorreta: 0),
+        titulo: "Quem desenvolveu a teoria da relatividade?",
+        respostas: ["Isaac Newton", "Albert Einstein", "Galileu Galilei"],
+        respostaCorreta: 1),
     Questao(
-        titulo: "Qual é a raiz quadrada de 64?", respostas: ["8", "6", "7"], respostaCorreta: 0),
+        titulo: "Qual é a raiz quadrada de 64?",
+        respostas: ["6", "7", "8"],
+        respostaCorreta: 2),
     Questao(
-        titulo: "Em qual continente fica o Egito?", respostas: ["África", "Ásia", "Europa"], respostaCorreta: 0),
+        titulo: "Em qual continente fica o Egito?",
+        respostas: ["Europa", "África", "Ásia"],
+        respostaCorreta: 1),
     Questao(
-        titulo: "Qual é o esporte mais popular do mundo?", respostas: ["Futebol", "Basquete", "Tênis"], respostaCorreta: 0),
+        titulo: "Qual é o esporte mais popular do mundo?",
+        respostas: ["Tênis", "Futebol", "Basquete"],
+        respostaCorreta: 1),
     Questao(
-        titulo: "Qual é a montanha mais alta do mundo?", respostas: ["Everest", "K2", "Kilimanjaro"], respostaCorreta: 0),
+        titulo: "Qual é a montanha mais alta do mundo?",
+        respostas: ["Kilimanjaro", "K2", "Everest"],
+        respostaCorreta: 2),
     Questao(
-        titulo: "Quem escreveu 'Dom Quixote'?", respostas: ["Miguel de Cervantes", "William Shakespeare", "Machado de Assis"], respostaCorreta: 0),
+        titulo: "Quem escreveu 'Dom Quixote'?",
+        respostas: ["Machado de Assis", "Miguel de Cervantes", "William Shakespeare"],
+        respostaCorreta: 1),
     Questao(
-        titulo: "Qual é o processador principal de um computador chamado?", respostas: ["CPU", "GPU", "RAM"], respostaCorreta: 0),
-    
+        titulo: "Qual é o processador principal de um computador chamado?",
+        respostas: ["RAM", "GPU", "CPU"],
+        respostaCorreta: 2),
 ]
